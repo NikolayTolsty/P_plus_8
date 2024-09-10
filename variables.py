4f90f26eb2e06de3217f5d7b4ebd5fe43d38be0d
NikolayTolsty <nikv.p@mail.ru>
2024-09-10 07:16:59 +0700

    Начальное состояние

---
+++ variables.py
@@ -0,0 +1,12 @@
+# переменные, описывающие объект 'animal'
+name='insect'
+print(name,type(name))
+
+warm_blood=False
+print(warm_blood,type(warm_blood))
+
+leg_count=6
+print(leg_count,type(leg_count))
+
+weight=2.48                           #g
+print(weight,type(weight))
